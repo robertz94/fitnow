d25dfe971f4d07a5bb090ee33cc6617e0e2c4af1
Robert Zhang <robertzhang94@gmail.com>
2021-05-03 23:12:55 +0200

    continued refactoring auth

--- controllers/category.js
+++ controllers/category.js
@@ -4,15 +4,6 @@ const jwt = require('jsonwebtoken')
 const Category = require('../models/category')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    console.log(authorization)
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-      return authorization.substring(7)
-    }
-    return null
-  }
-
 exports.getAll = async (request, response) => {
     const categories = await Category.find({})
     response.json(categories)
@@ -20,12 +11,8 @@ exports.getAll = async (request, response) => {
   
 exports.addCategory =  async (request, response) => {
     const body = request.body
-    const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
+
+  // const user = await User.findById(decodedToken.id)
   const category = new Category({
       name: body.name
   })
--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { getAll, addCategory } = require('../controllers/category')
+const { checkToken } = require('../controllers/auth')
 
 router.get('/', getAll)
 
-router.post('/', addCategory)
+router.post('/', checkToken, addCategory)
 
 module.exports = router
