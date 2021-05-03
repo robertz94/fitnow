c89f369ceb4744cf9b513d5721e7cc870f7e9063
Robert Zhang <robertzhang94@gmail.com>
2021-05-03 20:10:57 +0200

    {

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const userRoutes = require('./controllers/users')
+const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const PORT = process.env.PORT
 const url = 
-    `mongodb+srv://Admin:${process.env.PASSWORD}@cluster0.hsurz.mongodb.net/orders?retryWrites=true&w=majority`
+    `mongodb+srv://Admin:${process.env.PASSWORD}@cluster0.hsurz.mongodb.net/fitnow?retryWrites=true&w=majority`
 
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
     .then(console.log('Connected to Database'))
 
 const app = express()
 app.use(cors())
+app.use(express.json())
 
-app.use('/api/users', userRoutes)
+app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
